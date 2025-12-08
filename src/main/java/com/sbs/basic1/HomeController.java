65aa9df30aa9941661834dcff9b16f79bdc384e7
jiyuuuun <intesys1004@naver.com>
2025-12-08 20:07:57 +0900

    C:/Program Files/Git/usr/home/main2, /usr/home/main3 구현

--- src/main/java/com/sbs/basic1/HomeController.java
+++ src/main/java/com/sbs/basic1/HomeController.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// @Controller 의미
-// 개발자가 스프링부트한테 말한다.
-// 아래 있는 HomeController는 컨트롤러이다 라고 말하는 것.
 @Controller
 public class HomeController {
     @GetMapping("/home/main")
     @ResponseBody
     public String showHome() {
         return "안녕하세요.";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showHome2() {
+        return "환영합니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showHome3() {
+        return "스프링 부트입니다.";
     }
 }
