897f930a8af80224e89e8578244c3ca02cbba9d8
jiyuuuun <intesys1004@naver.com>
2025-12-13 00:37:56 +0900

    생성자 주입, 필드 주입

--- src/main/java/com/sbs/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/sbs/basic1/boundedContext/member/service/MemberService.java
@@ -3,7 +3,11 @@ package com.sbs.basic1.boundedContext.member.service;
 import com.sbs.basic1.boundedContext.base.rsData.RsData;
 import com.sbs.basic1.boundedContext.member.entity.Member;
 import com.sbs.basic1.boundedContext.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
 
+// @Component : 아래 클래스는 Ioc 컨테이너에 의해 생사소멸이 관리된다.
+// @Service : @Component와 같은 의미, 가독성 때문에 이렇게 표기
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
